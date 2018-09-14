9306a3dbd4ef353307d8d2172ddc7428237d3141
Kees Cook <keescook@chromium.org>
2018-09-14 15:37:20 -0700

    BACKPORT: Yama: Initialize as ordered LSM
    This converts Yama from being a direct "minor" LSM into an ordered LSM.
    
    Change-Id: Ie3d4cccfb950365b6a498114fa48ca06fcfe91fc
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/security.c
+++ security/security.c
@@ -275,7 +275,6 @@ int __init security_init(void)
 	 * Load minor LSMs, with the capability module always first.
 	 */
 	capability_add_hooks();
-	yama_add_hooks();
 
 	/* Load LSMs in specified order. */
 	ordered_lsm_init();
