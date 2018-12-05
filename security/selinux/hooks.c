181a9bcb19f22ea553f0630314275d665751926d
Al Viro <viro@zeniv.linux.org.uk>
2018-12-05 11:58:35 -0500

    BACKPORT: LSM: split ->sb_set_mnt_opts() out of ->sb_kern_mount()
    ... leaving the "is it kernel-internal" logics in the caller.
    
    Reviewed-by: David Howells <dhowells@redhat.com>
    Change-Id: I6edb516330dbff370507f0f6d7d1b4d7884d5246
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2848,18 +2848,10 @@ out_bad_option:
 	return -EINVAL;
 }
 
-static int selinux_sb_kern_mount(struct super_block *sb, int flags,
-				 struct security_mnt_opts *opts)
+static int selinux_sb_kern_mount(struct super_block *sb)
 {
 	const struct cred *cred = current_cred();
 	struct common_audit_data ad;
-	int rc = selinux_set_mnt_opts(sb, opts, 0, NULL);
-	if (rc)
-		goto out;
-
-	/* Allow all mounts performed by the kernel */
-	if (flags & (MS_KERNMOUNT | MS_SUBMOUNT))
-		goto out;
 
 	ad.type = LSM_AUDIT_DATA_DENTRY;
 	ad.u.dentry = sb->s_root;
