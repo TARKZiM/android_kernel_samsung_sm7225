fd86737e493c246e3b87e1149f71bc345e266252
Al Viro <viro@zeniv.linux.org.uk>
2018-12-23 18:55:56 -0500

    UPSTREAM: introduce fs_context methods
    Change-Id: Ia983a8efb6329e2e24a6daed8441dfdcaa2ca991
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/internal.h
+++ fs/internal.h
@@ -63,8 +63,6 @@ void dlog_hook_rmdir(struct dentry *, struct path *);
 /*
  * fs_context.c
  */
-extern int legacy_get_tree(struct fs_context *fc);
-extern int legacy_reconfigure(struct fs_context *fc);
 extern int parse_monolithic_mount_data(struct fs_context *, void *);
 extern void fc_drop_locked(struct fs_context *);
 
