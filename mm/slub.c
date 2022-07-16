611dcf2bd563c11d73d7a6ea7317531af0aaf0e8
frost. (frostg-012) <frost@nusantararom.org>
2022-07-16 08:52:42 +0700

    Merge tag 'v4.19.252' of git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into main
    This is the 4.19.252 stable release

--- mm/slub.c
+++ mm/slub.c
@@ -2438,6 +2438,7 @@ redo:
 
 	c->page = NULL;
 	c->freelist = NULL;
+	c->tid = next_tid(c->tid);
 }
 
 /*
@@ -2571,8 +2572,6 @@ static inline void flush_slab(struct kmem_cache *s, struct kmem_cache_cpu *c)
 {
 	stat(s, CPUSLAB_FLUSH);
 	deactivate_slab(s, c->page, c->freelist, c);
-
-	c->tid = next_tid(c->tid);
 }
 
 /*
@@ -2859,6 +2858,7 @@ redo:
 
 	if (!freelist) {
 		c->page = NULL;
+		c->tid = next_tid(c->tid);
 		stat(s, DEACTIVATE_BYPASS);
 		goto new_slab;
 	}
