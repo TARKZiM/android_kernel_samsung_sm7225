ef8aa831359cab3ce981a5df9c8df0f55755be0c
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-10-17 12:21:58 +0200

    sm7225: input: touchscreen: zt7548: Don't report KEY_BLACK_UI_GESTURE on FOD events
    Change-Id: I96c9a211fdc59080318d95daebe91b5982322867

--- drivers/input/touchscreen/zinitix/zt7548/zinitix_ts.c
+++ drivers/input/touchscreen/zinitix/zt7548/zinitix_ts.c
@@ -1715,11 +1715,6 @@ static void zt_ts_fod_event_report(struct zt_ts_info *info, struct point_info to
 			| ((touch_info.byte04.value_u8bit & 0xF0) >> 4);
 		info->scrub_y = ((touch_info.byte03.value_u8bit << 4) & 0xFF0)
 			| ((touch_info.byte04.value_u8bit & 0x0F));
-
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 1);
-		input_sync(info->input_dev);
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 0);
-		input_sync(info->input_dev);
 #ifdef CONFIG_SAMSUNG_PRODUCT_SHIP
 		input_info(true, &info->client->dev, "%s: FOD %s PRESS: %d\n", __func__,
 				touch_info.byte01.value_u8bit ? "NORMAL" : "LONG", info->scrub_id);
@@ -1738,11 +1733,6 @@ static void zt_ts_fod_event_report(struct zt_ts_info *info, struct point_info to
 			| ((touch_info.byte04.value_u8bit & 0xF0) >> 4);
 		info->scrub_y = ((touch_info.byte03.value_u8bit << 4) & 0xFF0)
 			| ((touch_info.byte04.value_u8bit & 0x0F));
-
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 1);
-		input_sync(info->input_dev);
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 0);
-		input_sync(info->input_dev);
 #ifdef CONFIG_SAMSUNG_PRODUCT_SHIP
 		input_info(true, &info->client->dev, "%s: FOD RELEASE: %d\n", __func__, info->scrub_id);
 #else
@@ -1756,11 +1746,6 @@ static void zt_ts_fod_event_report(struct zt_ts_info *info, struct point_info to
 			| ((touch_info.byte04.value_u8bit & 0xF0) >> 4);
 		info->scrub_y = ((touch_info.byte03.value_u8bit << 4) & 0xFF0)
 			| ((touch_info.byte04.value_u8bit & 0x0F));
-
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 1);
-		input_sync(info->input_dev);
-		input_report_key(info->input_dev, KEY_BLACK_UI_GESTURE, 0);
-		input_sync(info->input_dev);
 #ifdef CONFIG_SAMSUNG_PRODUCT_SHIP
 		input_info(true, &info->client->dev, "%s: FOD OUT: %d\n", __func__, info->scrub_id);
 #else
