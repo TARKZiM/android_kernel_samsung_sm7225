58d789d30c4f1f893ce1290e053874ca6d233176
Zenka <z.mail.6600@gmail.com>
2023-01-16 12:43:05 +0000

    Merge tag 'v4.19.262' stable release
    from https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
    
    Conflicts:
    	arch/arm/Kconfig
    	drivers/char/random.c
    	drivers/rpmsg/qcom_glink_native.c

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -2087,7 +2087,7 @@ bool udp_sk_rx_dst_set(struct sock *sk, struct dst_entry *dst)
 	struct dst_entry *old;
 
 	if (dst_hold_safe(dst)) {
-		old = xchg(&sk->sk_rx_dst, dst);
+		old = xchg((__force struct dst_entry **)&sk->sk_rx_dst, dst);
 		dst_release(old);
 		return old != dst;
 	}
@@ -2284,7 +2284,7 @@ int __udp4_lib_rcv(struct sk_buff *skb, struct udp_table *udptable,
 		char dstaddr[INET6_ADDRSTRLEN_NAP];
 		#endif
 
-		if (unlikely(sk->sk_rx_dst != dst))
+		if (unlikely(rcu_dereference(sk->sk_rx_dst) != dst))
 			udp_sk_rx_dst_set(sk, dst);
 		#ifdef CONFIG_KNOX_NCM
 		/* function to handle open flows with incoming udp packets */
@@ -2544,7 +2544,7 @@ int udp_v4_early_demux(struct sk_buff *skb)
 
 	skb->sk = sk;
 	skb->destructor = sock_efree;
-	dst = READ_ONCE(sk->sk_rx_dst);
+	dst = rcu_dereference(sk->sk_rx_dst);
 
 	if (dst)
 		dst = dst_check(dst, 0);
