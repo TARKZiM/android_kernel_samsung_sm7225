e0a94016c4af352d59b019002267e2ccd04d43a1
spectredev-007 <mohammad.galaxy07@gmail.com>
2023-01-29 11:40:51 +0000

    Merge tag 'v4.19.271' into custom
    This is the 4.19.271 stable release

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1172,7 +1172,6 @@ static struct inode *ext4_alloc_inode(struct super_block *sb)
 	ei->i_da_metadata_calc_len = 0;
 	ei->i_da_metadata_calc_last_lblock = 0;
 	spin_lock_init(&(ei->i_block_reservation_lock));
-	ext4_init_pending_tree(&ei->i_pending_tree);
 #ifdef CONFIG_QUOTA
 	ei->i_reserved_quota = 0;
 	memset(&ei->i_dquot, 0, sizeof(ei->i_dquot));
@@ -6611,7 +6610,6 @@ static void __exit ext4_exit_fs(void)
 	ext4_exit_pageio();
 	ext4_exit_post_read_processing();
 	ext4_exit_es();
-	ext4_exit_pending();
 }
 
 MODULE_AUTHOR("Remy Card, Stephen Tweedie, Andrew Morton, Andreas Dilger, Theodore Ts'o and others");
