da62636ff9631c4854be98d07a27f8b7fc342daa
Greg Kroah-Hartman <gregkh@google.com>
2023-12-21 12:12:36 +0000

    Merge 4.19.303 into android-4.19-stable
    Changes in 4.19.303
    	qca_debug: Prevent crash on TX ring changes
    	qca_debug: Fix ethtool -G iface tx behavior
    	qca_spi: Fix reset behavior
    	atm: solos-pci: Fix potential deadlock on &cli_queue_lock
    	atm: solos-pci: Fix potential deadlock on &tx_queue_lock
    	atm: Fix Use-After-Free in do_vcc_ioctl
    	net/rose: Fix Use-After-Free in rose_ioctl
    	qed: Fix a potential use-after-free in qed_cxt_tables_alloc
    	net: Remove acked SYN flag from packet in the transmit queue correctly
    	sign-file: Fix incorrect return values check
    	vsock/virtio: Fix unsigned integer wrap around in virtio_transport_has_space()
    	net: stmmac: Handle disabled MDIO busses from devicetree
    	appletalk: Fix Use-After-Free in atalk_ioctl
    	Revert "PCI: acpiphp: Reassign resources on bridge if necessary"
    	cred: switch to using atomic_long_t
    	blk-throttle: fix lockdep warning of "cgroup_mutex or RCU read lock required!"
    	bcache: avoid oversize memory allocation by small stripe_size
    	bcache: add code comments for bch_btree_node_get() and __bch_btree_node_alloc()
    	bcache: avoid NULL checking to c->root in run_cache_set()
    	platform/x86: intel_telemetry: Fix kernel doc descriptions
    	HID: add ALWAYS_POLL quirk for Apple kb
    	HID: hid-asus: reset the backlight brightness level on resume
    	HID: multitouch: Add quirk for HONOR GLO-GXXX touchpad
    	asm-generic: qspinlock: fix queued_spin_value_unlocked() implementation
    	net: usb: qmi_wwan: claim interface 4 for ZTE MF290
    	HID: hid-asus: add const to read-only outgoing usb buffer
    	perf: Fix perf_event_validate_size() lockdep splat
    	ext4: prevent the normalized size from exceeding EXT_MAX_BLOCKS
    	arm64: mm: Always make sw-dirty PTEs hw-dirty in pte_modify
    	team: Fix use-after-free when an option instance allocation fails
    	ring-buffer: Fix memory leak of free page
    	mmc: block: Be sure to wait while busy in CQE error recovery
    	powerpc/ftrace: Create a dummy stackframe to fix stack unwind
    	powerpc/ftrace: Fix stack teardown in ftrace_no_trace
    	Linux 4.19.303
    
    Change-Id: I209240976b6ff49ae89b0a77e777041e376416d9
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/cred.c
+++ kernel/cred.c
@@ -117,17 +117,17 @@ static void put_cred_rcu(struct rcu_head *rcu)
 
 #ifdef CONFIG_DEBUG_CREDENTIALS
 	if (cred->magic != CRED_MAGIC_DEAD ||
-	    atomic_read(&cred->usage) != 0 ||
+	    atomic_long_read(&cred->usage) != 0 ||
 	    read_cred_subscribers(cred) != 0)
 		panic("CRED: put_cred_rcu() sees %p with"
-		      " mag %x, put %p, usage %d, subscr %d\n",
+		      " mag %x, put %p, usage %ld, subscr %d\n",
 		      cred, cred->magic, cred->put_addr,
-		      atomic_read(&cred->usage),
+		      atomic_long_read(&cred->usage),
 		      read_cred_subscribers(cred));
 #else
-	if (atomic_read(&cred->usage) != 0)
-		panic("CRED: put_cred_rcu() sees %p with usage %d\n",
-		      cred, atomic_read(&cred->usage));
+	if (atomic_long_read(&cred->usage) != 0)
+		panic("CRED: put_cred_rcu() sees %p with usage %ld\n",
+		      cred, atomic_long_read(&cred->usage));
 #endif
 
 	security_cred_free(cred);
@@ -150,8 +150,8 @@ static void put_cred_rcu(struct rcu_head *rcu)
  */
 void __put_cred(struct cred *cred)
 {
-	kdebug("__put_cred(%p{%d,%d})", cred,
-	       atomic_read(&cred->usage),
+	kdebug("__put_cred(%p{%ld,%d})", cred,
+	       atomic_long_read(&cred->usage),
 	       read_cred_subscribers(cred));
 #ifdef CONFIG_KDP_CRED
 	if (is_kdp_protect_addr((unsigned long)cred))
@@ -159,7 +159,7 @@ void __put_cred(struct cred *cred)
 	else
 #endif
 
-	BUG_ON(atomic_read(&cred->usage) != 0);
+	BUG_ON(atomic_long_read(&cred->usage) != 0);
 #ifdef CONFIG_DEBUG_CREDENTIALS
 	BUG_ON(read_cred_subscribers(cred) != 0);
 	cred->magic = CRED_MAGIC_DEAD;
@@ -198,8 +198,8 @@ void exit_creds(struct task_struct *tsk)
 {
 	struct cred *cred;
 
-	kdebug("exit_creds(%u,%p,%p,{%d,%d})", tsk->pid, tsk->real_cred, tsk->cred,
-	       atomic_read(&tsk->cred->usage),
+	kdebug("exit_creds(%u,%p,%p,{%ld,%d})", tsk->pid, tsk->real_cred, tsk->cred,
+	       atomic_long_read(&tsk->cred->usage),
 	       read_cred_subscribers(tsk->cred));
 
 	cred = (struct cred *) tsk->real_cred;
@@ -247,7 +247,7 @@ const struct cred *get_task_cred(struct task_struct *task)
 	do {
 		cred = __task_cred((task));
 		BUG_ON(!cred);
-	} while (!atomic_inc_not_zero(&((struct cred *)cred)->usage));
+	} while (!atomic_long_inc_not_zero(&((struct cred *)cred)->usage));
 #endif
 	rcu_read_unlock();
 	return cred;
@@ -265,7 +265,7 @@ struct cred *cred_alloc_blank(void)
 	if (!new)
 		return NULL;
 
-	atomic_set(&new->usage, 1);
+	atomic_long_set(&new->usage, 1);
 #ifdef CONFIG_DEBUG_CREDENTIALS
 	new->magic = CRED_MAGIC;
 #endif
@@ -312,7 +312,7 @@ struct cred *prepare_creds(void)
 	memcpy(new, old, sizeof(struct cred));
 
 	new->non_rcu = 0;
-	atomic_set(&new->usage, 1);
+	atomic_long_set(&new->usage, 1);
 	set_cred_subscribers(new, 0);
 	get_group_info(new->group_info);
 	get_uid(new->user);
@@ -400,8 +400,8 @@ int copy_creds(struct task_struct *p, unsigned long clone_flags)
 		p->real_cred = get_cred(p->cred);
 		get_cred(p->cred);
 		alter_cred_subscribers(p->cred, 2);
-		kdebug("share_creds(%p{%d,%d})",
-		       p->cred, atomic_read(&p->cred->usage),
+		kdebug("share_creds(%p{%ld,%d})",
+		       p->cred, atomic_long_read(&p->cred->usage),
 		       read_cred_subscribers(p->cred));
 		atomic_inc(&p->cred->user->processes);
 		return 0;
@@ -505,8 +505,8 @@ int commit_creds(struct cred *new)
 	struct task_struct *task = current;
 	const struct cred *old = task->real_cred;
 
-	kdebug("commit_creds(%p{%d,%d})", new,
-	       atomic_read(&new->usage),
+	kdebug("commit_creds(%p{%ld,%d})", new,
+	       atomic_long_read(&new->usage),
 	       read_cred_subscribers(new));
 
 	BUG_ON(task->cred != old);
@@ -520,7 +520,7 @@ int commit_creds(struct cred *new)
 		BUG_ON(ROCRED_UC_READ(new) < 1);
 	else
 #endif
-	BUG_ON(atomic_read(&new->usage) < 1);
+	BUG_ON(atomic_long_read(&new->usage) < 1);
 
 	get_cred(new); /* we will require a ref for the subj creds too */
 
@@ -612,8 +612,8 @@ EXPORT_SYMBOL(commit_creds);
  */
 void abort_creds(struct cred *new)
 {
-	kdebug("abort_creds(%p{%d,%d})", new,
-	       atomic_read(&new->usage),
+	kdebug("abort_creds(%p{%ld,%d})", new,
+	       atomic_long_read(&new->usage),
 	       read_cred_subscribers(new));
 
 #ifdef CONFIG_DEBUG_CREDENTIALS
@@ -624,7 +624,7 @@ void abort_creds(struct cred *new)
 		BUG_ON(ROCRED_UC_READ(new) < 1);
 	else
 #endif
-	BUG_ON(atomic_read(&new->usage) < 1);
+	BUG_ON(atomic_long_read(&new->usage) < 1);
 	put_cred(new);
 }
 EXPORT_SYMBOL(abort_creds);
@@ -646,8 +646,8 @@ const struct cred *override_creds(const struct cred *new)
 #ifdef CONFIG_KDP_CRED
 	struct cred *new = *cnew;
 #endif
-	kdebug("override_creds(%p{%d,%d})", new,
-	       atomic_read(&new->usage),
+	kdebug("override_creds(%p{%ld,%d})", new,
+	       atomic_long_read(&new->usage),
 	       read_cred_subscribers(new));
 
 	validate_creds(old);
@@ -684,8 +684,8 @@ const struct cred *override_creds(const struct cred *new)
 #endif /* CONFIG_KDP_CRED */
 	alter_cred_subscribers(old, -1);
 
-	kdebug("override_creds() = %p{%d,%d}", old,
-	       atomic_read(&old->usage),
+	kdebug("override_creds() = %p{%ld,%d}", old,
+	       atomic_long_read(&old->usage),
 	       read_cred_subscribers(old));
 	return old;
 }
@@ -706,8 +706,8 @@ void revert_creds(const struct cred *old)
 {
 	const struct cred *override = current->cred;
 
-	kdebug("revert_creds(%p{%d,%d})", old,
-	       atomic_read(&old->usage),
+	kdebug("revert_creds(%p{%ld,%d})", old,
+	       atomic_long_read(&old->usage),
 	       read_cred_subscribers(old));
 
 	validate_creds(old);
@@ -770,7 +770,7 @@ struct cred *prepare_kernel_cred(struct task_struct *daemon)
 
 	*new = *old;
 	new->non_rcu = 0;
-	atomic_set(&new->usage, 1);
+	atomic_long_set(&new->usage, 1);
 	set_cred_subscribers(new, 0);
 	get_uid(new->user);
 	get_user_ns(new->user_ns);
@@ -893,8 +893,8 @@ static void dump_invalid_creds(const struct cred *cred, const char *label,
 	       cred == tsk->cred ? "[eff]" : "");
 	printk(KERN_ERR "CRED: ->magic=%x, put_addr=%p\n",
 	       cred->magic, cred->put_addr);
-	printk(KERN_ERR "CRED: ->usage=%d, subscr=%d\n",
-	       atomic_read(&cred->usage),
+	printk(KERN_ERR "CRED: ->usage=%ld, subscr=%d\n",
+	       atomic_long_read(&cred->usage),
 	       read_cred_subscribers(cred));
 	printk(KERN_ERR "CRED: ->*uid = { %d,%d,%d,%d }\n",
 		from_kuid_munged(&init_user_ns, cred->uid),
@@ -966,9 +966,9 @@ EXPORT_SYMBOL(__validate_process_creds);
  */
 void validate_creds_for_do_exit(struct task_struct *tsk)
 {
-	kdebug("validate_creds_for_do_exit(%p,%p{%d,%d})",
+	kdebug("validate_creds_for_do_exit(%p,%p{%ld,%d})",
 	       tsk->real_cred, tsk->cred,
-	       atomic_read(&tsk->cred->usage),
+	       atomic_long_read(&tsk->cred->usage),
 	       read_cred_subscribers(tsk->cred));
 
 	__validate_process_creds(tsk, __FILE__, __LINE__);
